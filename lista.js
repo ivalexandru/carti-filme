a5c4b1ef3ea3364f0c949b788494a207f4a7ae04
ivalexandru <ivalexandru.1984@gmail.com>
2025-07-02 10:03:49 +0300

    Create lista.js
    model

---
+++ lista.js
@@ -0,0 +1,119 @@
+let carti_incepute = {
+
+    andy_weir: "project hail mary",
+    andy_weir: "project hail mary",
+
+
+}
+
+
+let seriale_incepute = {
+
+    andy_weir2025: "nume serial S1"
+
+}
+
+
+
+
+
+let carti = {
+    zece: {
+        frank_herbert: "dune", 
+    },
+
+    noua: {
+        xx: "xx", 
+
+    },
+
+    opt: {
+        xx: "xx", 
+
+
+    },
+
+    sapte: {
+        xx: "xx", 
+
+
+    },
+
+    sub_sapte: {
+        xx: "xx", 
+
+        
+    }
+};
+
+
+
+
+let filme = {
+    zece: {
+        dune: 1995,
+    },
+
+    noua: {
+        xx: 1999, 
+
+    },
+
+    opt: {
+        xx: 2024, 
+
+
+    },
+
+    sapte: {
+        xx: 2025, 
+
+
+    },
+
+    sub_sapte: {
+        xx: 2024, 
+
+        
+    },
+};
+
+
+
+let seriale = {
+    zece: {
+        dune: 1995
+    },
+
+    noua: {
+        xx: 1999, 
+
+    },
+
+    opt: {
+        xx: 2024, 
+
+
+    },
+
+    sapte: {
+        xx: 2025, 
+
+
+    },
+
+    sub_sapte: {
+        xx: 2024, 
+
+        
+    },
+};
+
+
+
+
+
+
+
+
+
