45c7f160e7accb2a6c7bad1cf051dbc1adb9fe4d
ivalexandru <ivalexandru.1984@gmail.com>
2025-07-09 13:45:54 +0300

    Update books.sql

--- books.sql
+++ books.sql
@@ -1,13 +1,36 @@
-BEGIN;
-CREATE TABLE books (id, year, finished, book_name, author, genre, comment, list_all_genres, list_specific_genre);
-INSERT INTO books VALUES (1, 1965, "yes", "Dune", "Frank Herbert", "SF", "amazing", "top_100_all_time", "top_100_SF");
-INSERT INTO books VALUES (2, 1951, "yes", "Foundation", "Isaac Asimov", "SF", "amazing", "top_100_all_time", "top_100_SF");
-INSERT INTO books VALUES (3, 1968, "yes", "2001: A Space Odyssey", "Arthur C. Clarke", "SF", "amazing", "top_100_all_time", "top_100_SF");
-INSERT INTO books VALUES (4, 2006, "no", "The Bastard of Istanbul", "Elif Shafak", "novel", "not for me", "not_for_me", "not_for_me");
-INSERT INTO books VALUES (5, 2008, "yes", "The Museum of Innocence", "Orhan Pamuk", "novel", "good", "romance_ish", "love_but_twisted");
-COMMIT;
+-- no trailing comma, else err!!
+-- CREATE TABLE books (
+--     name TEXT NOT NULL,
+--     rating REAL NOT NULL,
+--     author TEXT,
+--     genre TEXT,
+--     comment TEXT,
+--     lists TEXT
+-- );
+
+INSERT INTO books (name, rating, author, genre)
+VALUES
+-- ("Dune", 10, "Frank Herbert", "SF" ),
+-- ("Foundation", 9.6, "Isaac Asimov", "SF"),
+-- ("2001: A Space Odyssey", 9.7, "Arthur C. Clarke", "SF"),
+-- ("The Museum of Innocence", 7.7, "Orhan Pamuk", "novel"),
+-- ("Silo (Wool)", 6.6, "Hugh Howey", "SF");
 
 
 
+-- CREATE TABLE unfinished_books (
+--     name TEXT NOT NULL,
+--     rating INTEGER,
+--     author TEXT,
+--     comment TEXT
+-- );
 
+-- INSERT INTO unfinished_books (name, rating, author, comment)
+-- VALUES
+-- ("The Bastard of Istanbul", 4, "Elif Shafak", 'not for me');
+
+
+-- display
+SELECT * FROM books;
+SELECT * FROM unfinished_books;
 
